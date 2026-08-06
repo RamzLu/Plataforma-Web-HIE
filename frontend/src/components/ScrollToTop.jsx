66662085da27fe643f7d908f5ee0befbfd919a90
RamzLu <luanaabigail168@gmail.com>
2026-08-06 10:03:55 -0300

    comentarios

--- frontend/src/components/ScrollToTop.jsx
+++ frontend/src/components/ScrollToTop.jsx
@@ -2,10 +2,8 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  // Extraemos la ruta actual (pathname)
   const { pathname } = useLocation();
 
-  // Cada vez que el pathname cambie, ejecutamos el scroll hacia arriba
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
