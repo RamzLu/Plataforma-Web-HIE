97e54372af6d298f6ed511737c9671ede3942314
Stella <bernardstella0704@gmail.com>
2026-08-19 12:44:54 -0300

    fix: variables de entorno. Conexion a supabase realizada

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,14 +1,7 @@
 import "dotenv/config.js";
 
-const requiredVariables = [
-  "PORT",
-  "NODE_ENV",
-  "DB_HOST",
-  "DB_PORT",
-  "DB_USER",
-  "DB_PASSWORD",
-  "DB_NAME",
-];
+console.log(process.env.DATABASE_URL);
+const requiredVariables = ["PORT", "NODE_ENV", "DATABASE_URL", "JWT_SECRET"];
 
 requiredVariables.forEach((variable) => {
   if (!process.env[variable]) {
@@ -19,12 +12,6 @@ requiredVariables.forEach((variable) => {
 export const envs = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
-
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_NAME: process.env.DB_NAME,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-
-  //JWT_SECRET: process.env.JWT_SECRET,
+  DATABASE_URL: process.env.DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
