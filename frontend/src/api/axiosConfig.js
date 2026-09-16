65e92f3f2bbe5eb7fb4fc8605afd38f1e81a7f89
RamzLu <luanaabigail168@gmail.com>
2026-09-16 12:33:55 -0300

    cambios

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,20 +1,12 @@
 import axios from "axios";
 
-// Instancia base de Axios
 const api = axios.create({
-  // En producción, esto debería apuntar a import.meta.env.VITE_API_URL
-  baseURL: "http://localhost:3000/api",
-  timeout: 15000, // Tiempo de espera máximo de 15 segundos
+baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  timeout: 15000, 
 });
 
-// INTERCEPTOR: Se ejecuta ANTES de que cualquier petición salga al backend
 api.interceptors.request.use(
   (config) => {
-    // Aquí es donde inyectarás el token de Keycloak en el futuro para proteger el CMS
-    // const token = localStorage.getItem("keycloak_token"); // O de donde saques el token
-    // if (token) {
-    //   config.headers.Authorization = `Bearer ${token}`;
-    // }
     return config;
   },
   (error) => {
